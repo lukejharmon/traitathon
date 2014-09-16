d3af28d271e9cdffc8116fc329cd022dbd93da1a
Luke Harmon <lukeh@uidaho.edu>
2014-09-16 13:58:53 -0700

    continuous character correlation script

--- heliconia/continuousCharacter_noOpenTree_example.R
+++ heliconia/continuousCharacter_noOpenTree_example.R
@@ -1,9 +1,17 @@
 # First set the correct working directory
-# setwd("~/Documents/openTreeHackathon/traitathon/anolis")
+# setwd("~/Documents/openTreeHackathon/traitathon/heliconia")
 require(aRbor)
 
-# read in anolis phylogenetic tree
+# read in heliconia phylogenetic tree
+heliTree<-read.tree("HeliconiaSelectEdit.phy")
+plot(heliTree)
 
 # get the data matrix
+heliData<-read.csv("heliconia1.csv")
+for(i in 7:13) 
+	heliData[,i]<-as.numeric(as.character(heliData[,i]))
 
 # glue traits and trees together
+heliContinuousTd<-make.treedata(heliTree, heliData[,c(1,7:13)])
+
+continuousCorrelation(heliContinuousTd)
