c952d54c4899743ba0619b5379f36405906d3f4c
Luke Harmon <lukeh@uidaho.edu>
2014-09-16 13:12:03 -0700

    heliconia cc script

---
+++ heliconia/continuousCharacter_noOpenTree_example.R
@@ -0,0 +1,9 @@
+# First set the correct working directory
+# setwd("~/Documents/openTreeHackathon/traitathon/anolis")
+require(aRbor)
+
+# read in anolis phylogenetic tree
+
+# get the data matrix
+
+# glue traits and trees together
