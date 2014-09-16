52450e6800dec04e3cfc0b06fb7a47d97e49bf24
ShanKothari <quercusacerifolia@gmail.com>
2014-09-16 14:53:28 -0500

    ape

--- rAnalysis/RImportData.r
+++ rAnalysis/RImportData.r
@@ -5,6 +5,7 @@ get_traits<-function(trait_file_path){
 }
 
 get_tree<-function(tree_file_path){
+	require(ape)
 	## this is to be replaced with a way of calling trees
 	## from Open Tree given species names
 	tree<-read.tree(tree_file_path)
