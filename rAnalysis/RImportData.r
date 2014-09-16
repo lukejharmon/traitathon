c2ee7061c291f7a00b39260a18e0abf1fe4ee637
ShanKothari <quercusacerifolia@gmail.com>
2014-09-16 14:52:14 -0500

    Importing data script (preliminary)

---
+++ rAnalysis/RImportData.r
@@ -0,0 +1,11 @@
+get_traits<-function(trait_file_path){
+	## this is to be replaced with a way of calling traits
+	## given species names
+	trait_mat<-read.csv(trait_file_path)
+}
+
+get_tree<-function(tree_file_path){
+	## this is to be replaced with a way of calling trees
+	## from Open Tree given species names
+	tree<-read.tree(tree_file_path)
+}
