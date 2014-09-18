1a836c6021ef978334ceec154e246c9f765c4b89
Jorrit Poelen <jhpoelen@gmail.com>
2014-09-18 16:28:33 -0400

    fix bugs in example

--- GloBI-OTL/PlotInteractionTreesAndNetworkExample.R
+++ GloBI-OTL/PlotInteractionTreesAndNetworkExample.R
@@ -13,21 +13,21 @@ get_tree = function(ids) {
 
 # get derived trees from OpenTree of Life 
 sourceTree <- get_tree(interactionTable$source_id)
-targetTtree <- get_tree(interactionTable$target_id)
+targetTree <- get_tree(interactionTable$target_id)
 
 # start plotting
 par(mfrow=c(1,3))
 
-plot(source_tree, main='source taxa')
-plot(target_tree, main='target tree')
+plot(sourceTree, main='source taxa')
+plot(targetTree, main='target tree')
 
 # transform interaction table to edges and vertices
-edges <- cbind(interactions$source_id, interactions$target_id)
-ids <- c(interactions$source_id, interactions$target_id)
-labels <- c(interactions$source_name, interactions$target_name)
+edges <- cbind(interactionTable$source_id, interactionTable$target_id)
+ids <- c(interactionTable$source_id, interactionTable$target_id)
+labels <- c(interactionTable$source_name, interactionTable$target_name)
 vertices <- unique(cbind(ids, labels))
 graph <- igraph::graph.data.frame(d= edges, directed=T, vertices = vertices)
 igraph::V(graph)$label <- vertices[,2]
 
 # plot interaction network
-igraph::plot.igraph(graph, layout=layout.circle, vertex.color='green', vertex.size=5, main='interactions')
+igraph::plot.igraph(graph, layout=igraph::layout.circle, vertex.color='green', vertex.size=5, main='interactions')
