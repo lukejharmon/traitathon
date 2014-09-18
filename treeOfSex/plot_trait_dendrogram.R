db3ad4db4a442eb3b79cac4cf5b9d3b3311b3692
Alex Harkess <aharkess@plantbio.uga.edu>
2014-09-18 10:05:01 -0400

    added legend location parameter on plot_trait_dendrogram

--- treeOfSex/plot_trait_dendrogram.R
+++ treeOfSex/plot_trait_dendrogram.R
@@ -10,7 +10,7 @@
 #' Author: Alex Harkess <br />
 #' Example: plot_trait_dendrogram(tree, charactermatrix, tree_type="fan", trait_header="island")
 
-plot_trait_dendrogram <- function(apetree, character_vector, tree_type="phylogram", binary_trait=FALSE, legend=TRUE, legend_cex=0.75, tiplabel_cex=0.4) {
+plot_trait_dendrogram <- function(apetree, character_vector, tree_type="phylogram", binary_trait=FALSE, legend=TRUE, legend_cex=0.75, tiplabel_cex=0.4, legend_loc="bottomleft") {
 	
 	
 	plot(apetree, show.tip.label=FALSE, no.margin=FALSE, type=tree_type)
@@ -22,15 +22,15 @@ plot_trait_dendrogram <- function(apetree, character_vector, tree_type="phylogra
 		tiplabels(col=tipcols[character_vector], pch=19, cex=tiplabel_cex)
 		
 		if(isTRUE(legend)) {
-			legend("bottomright", as.character(unique(character_vector)), fill=unique(tipcols[character_vector]), bty="n", border=FALSE, cex=legend_cex)
+			legend(legend_loc, as.character(unique(character_vector)), fill=unique(tipcols[character_vector]), bty="n", border=FALSE, cex=legend_cex)
 		}
 	}
 	
 	# discrete plot colors and legend
 	else {
-		tiplabels(col=character_vector, pch=19)
+		tiplabels(col=character_vector, pch=19, cex=tiplabel_cex)
 		if(isTRUE(legend)) {
-			legend("bottomright", as.character(unique(character_vector)), fill=as.numeric(unique(character_vector)), bty="n", border=FALSE, cex=legend_cex)
+			legend(legend_loc, as.character(unique(character_vector)), fill=as.numeric(unique(character_vector)), bty="n", border=FALSE, cex=legend_cex)
 		}
 	}
 }
