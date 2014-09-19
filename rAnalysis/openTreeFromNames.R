28bdbd14fd4060faff9914cbcd175c35f6f423e7
ShanKothari <quercusacerifolia@gmail.com>
2014-09-19 10:26:37 -0500

    clear out un-needed gopher-lice stuff

--- rAnalysis/openTreeFromNames.R
+++ rAnalysis/openTreeFromNames.R
@@ -9,12 +9,11 @@ OT.from.names<-function(species_names,underscores=F){
   # if underscores==T, replaces with a space to enable faster exact
   # rather than fuzzy matching; also discards anything other than genus and species
   if(underscores==T){
-    split_names<-strsplit(species_names,split="_")
-    species_names<-sapply(split_names, function(x) paste(x[1],x[2],sep=" "))
-  # if underscores==F, just discards anything past genus and species
-  } else {split_names<-strsplit(species_names,sep=" ")
-    species_names<-sapply(split_names, function(x) paste(x[1],x[2],sep=" "))
+    species_names<-gsub(species_names,pattern="_",replacement=" ")
   }
+  # discards anything past genus and species
+  split_names<-strsplit(species_names,split=" ")
+  species_names<-sapply(split_names, function(x) paste(x[1],x[2],sep=" "))
   # retrieves OpenTree IDs for species
   name_matches<-tnrs_match_names(species_names)
   ottID<-name_matches[,4]
