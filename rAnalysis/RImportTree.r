5b93991d5ce04de52aeeb36a782692cc7d2b4652
ShanKothari <quercusacerifolia@gmail.com>
2014-09-18 19:54:16 -0500

    delete useless import tree function

--- rAnalysis/RImportTree.r
+++
@@ -1,9 +0,0 @@
-## dummy code to be filled in with method to draw trees from OpenTree
-
-get_tree<-function(tree_file_path){
-	require(ape)
-	## this is to be replaced with a way of calling trees
-	## from Open Tree given species names
-	tree<-read.tree(tree_file_path)
-	return(tree)
-}
